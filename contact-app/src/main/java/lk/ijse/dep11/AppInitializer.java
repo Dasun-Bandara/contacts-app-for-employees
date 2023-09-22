662c9b39427715d44956e709833674ddc1fdbe14
Dasun Bandara <dasun8517@gmail.com>
2023-09-22 16:10:21 +0530

    UI-design

---
+++ contact-app/src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Scene mainScene = new Scene(FXMLLoader.load(getClass().getResource("/view/MainForm.fxml")));
+        primaryStage.setScene(mainScene);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("CONTACT");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+
+    }
+}
---
+++ contact-app/src/main/java/lk/ijse/dep11/Controller/MainFormController.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep11.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+    public AnchorPane root;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtContact;
+    public Button btnSave;
+    public Button btnDelete;
+    public TableView tblEmployee;
+    public TextField txtSearch;
+    public Button btnNew;
+
+    public void btnSaveOnAction(ActionEvent event) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent event) {
+    }
+
+    public void btnNewOnAction(ActionEvent event) {
+
+    }
+}
